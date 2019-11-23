17b68180a5923d202ae1387c251790b1ec629651
Rachael Shaw <rachaelleeshaw@gmail.com>
2019-11-22 18:53:20 -0600

    update config file

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://agile-peak-15686.herokuapp.com/api'
+  API_ENDPOINT: 'https://agile-peak-15686.herokuapp.com/api',
+  TOKEN_KEY: 'blogful-client-auth-token',
 }
